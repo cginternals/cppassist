33a0dca14c64373aa53d145b6f5d2ad1da712aec
Willy Scheibel <willy.scheibel@hpi.de>
2016-05-31 17:58:19 +0200

    Fix obsolete assertion

--- source/cppassist/source/logging/LogMessageBuilder.cpp
+++ source/cppassist/source/logging/LogMessageBuilder.cpp
@@ -17,7 +17,6 @@ LogMessageBuilder::LogMessageBuilder(LogMessage::Level level, AbstractLogHandler
 , m_context(context)
 , m_stream(new std::stringstream)
 {
-    assert(handler != nullptr);
 }
 
 LogMessageBuilder::LogMessageBuilder(const LogMessageBuilder & builder)
