b41244e5b0ed64021e3a379d30937c7bcb52ef28
Willy Scheibel <willy.scheibel@hpi.de>
2016-12-20 10:06:57 +0100

    Move common code to superclass

--- source/cppassist/source/logging/ConsoleLogHandler.cpp
+++ source/cppassist/source/logging/ConsoleLogHandler.cpp
@@ -10,37 +10,11 @@ namespace cppassist
 
 void ConsoleLogHandler::handle(const LogMessage & message)
 {
-    if (LogMessage::Info > message.level())
+    // For critical, error, and warning level
+    if (message.level() < LogMessage::Info)
         std::cerr << messagePrefix(message) << message.message() << std::endl;
     else
         std::cout << messagePrefix(message) << message.message() << std::endl;
-}
-
-std::string ConsoleLogHandler::messagePrefix(const LogMessage & message)
-{
-    std::string prefix = levelString(message.level());
-
-    if (!message.context().empty())
-    {
-        if (!prefix.empty())
-            prefix = prefix + " ";
-
-        prefix = prefix + "[" + message.context() + "]";
-    }
-
-    if (prefix.empty())
-        return prefix;
-
-    return prefix + ": ";
-}
-
-std::string ConsoleLogHandler::levelString(int level)
-{
-         if (level == LogMessage::Critical) return "#critical";
-    else if (level == LogMessage::Error)    return "#error";
-    else if (level == LogMessage::Warning)  return "#warning";
-    else if (level >= LogMessage::Debug)    return "#debug";
-    else                                    return "";
 }
 
 
--- source/cppassist/source/logging/FileLogHandler.cpp
+++ source/cppassist/source/logging/FileLogHandler.cpp
@@ -20,33 +20,4 @@ void FileLogHandler::handle(const LogMessage & message)
     logstream << messagePrefix(message) << message.message() << std::endl;
 }
 
-std::string FileLogHandler::messagePrefix(const LogMessage & message)
-{
-    std::string prefix = levelString(message.level());
-
-
-    if (!message.context().empty())
-    {
-        if (!prefix.empty())
-            prefix = prefix + " ";
-
-        prefix = prefix + "[" + message.context() + "]";
-    }
-
-    if (prefix.empty())
-        return prefix;
-
-    return prefix + ": ";
-}
-
-std::string FileLogHandler::levelString(int level)
-{
-         if (level == LogMessage::Critical) return "#critical";
-    else if (level == LogMessage::Error)    return "#error";
-    else if (level == LogMessage::Warning)  return "#warning";
-    else if (level >= LogMessage::Debug)    return "#debug";
-    else                                    return "";
-}
-
-
 } // namespace cppassist
