24ab1dadce393075db5f232a261a1d1185ad6838
Willy Scheibel <willy.scheibel@hpi.de>
2017-07-04 17:54:11 +0200

    Add TypeList module and class

---
+++ source/cppassist/include/cppassist/typelist/TypeList.h
@@ -0,0 +1,68 @@
+
+#pragma once
+
+
+namespace cppassist
+{
+
+
+/**
+*  @brief
+*    A compile-time list of types.
+*
+*    This is the default template for template specializations.
+*/
+template <typename... Types>
+class TypeList;
+
+/**
+*  @brief
+*    A compile-time list of types.
+*
+*    This is the template specialization for non-zero type lists.
+*    There is an interface to call a template operator() on a Functor for each type in the type list.
+*/
+template <typename T, typename... Types>
+class TypeList<T, Types...>
+{
+public:
+    /**
+    *  @brief
+    *    Call the template operator() on the Functor for each type.
+    *
+    *  @param[in] callback
+    *    The functor, supporting a templated operator()
+    */
+    template <typename Functor>
+    static void apply(Functor && callback);
+};
+
+/**
+*  @brief
+*    A compile-time list of types.
+*
+*    This is the template specialization for a zero type list.
+*    It is used as the recursion break condition.
+*/
+template <>
+class TypeList<>
+{
+public:
+    /**
+    *  @brief
+    *    Call the template operator() on the Functor for each type.
+    *
+    *    As this is the empty type list, no operator is called.
+    *
+    *  @param[in] callback
+    *    The functor, supporting a templated operator()
+    */
+    template <typename Functor>
+    static void apply(Functor && callback);
+};
+
+
+} // namespace cppassist
+
+
+#include <cppassist/typelist/TypeList.inl>
---
+++ source/cppassist/include/cppassist/typelist/TypeList.inl
@@ -0,0 +1,27 @@
+
+#pragma once
+
+
+#include <utility>
+
+
+namespace cppassist
+{
+
+
+template <typename T, typename... Types>
+template <typename Functor>
+void TypeList<T, Types...>::apply(Functor && callback)
+{
+    (callback.template operator()<T>)();
+
+    TypeList<Types...>::apply(std::forward<Functor>(callback));
+}
+
+template <typename Functor>
+void TypeList<>::apply(Functor && /*callback*/)
+{
+}
+
+
+} // namespace cppassist
