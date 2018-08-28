a89351cebffbd2c66192ae38b4960a38cebd5704
Tobias Knöschke <tobias.knoeschke@student.hpi.uni-potsdam.de>
2018-08-28 09:59:02 +0200

    Add documentation to offsetof.h

--- source/cppassist/include/cppassist/memory/offsetof.h
+++ source/cppassist/include/cppassist/memory/offsetof.h
@@ -9,6 +9,16 @@ namespace cppassist
 {
 
 
+/**
+*  @brief
+*    Determines the offset of a member within a class
+*
+*  @param[in] member
+*    Pointer to class member
+*
+*  @return
+*    Offset of member
+*/
 template <typename Class, typename Type>
 std::ptrdiff_t offset(Type Class::*member);
 
