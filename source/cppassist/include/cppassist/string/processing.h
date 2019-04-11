d00a5a25e2a7bafcafce48e8759a5fbc3f2c6d59
Willy Scheibel <willy.scheibel@hpi.de>
2019-04-11 22:44:39 +0200

    Add simple line part processing (e.g., to parse CSV)

---
+++ source/cppassist/include/cppassist/string/processing.h
@@ -0,0 +1,39 @@
+
+#pragma once
+
+
+#include <functional>
+#include <string>
+
+#include <cppassist/cppassist_api.h>
+
+
+namespace cppassist
+{
+namespace string
+{
+
+
+//@{
+/**
+*  @brief
+*    Splits the line at delimiter (while regarding the symmetric escapeCharacter) and calls the callback for each part.
+*
+*  @param[in] line
+*    The line to parse
+*  @param[in] start_pos
+*    The start pos within the line for parsing
+*  @param[in] delimiter
+*    The delimiter for adjacent parts within the line
+*  @param[in] escapeCharacter
+*    The character for inline escaping (while escaped, the delimiter is ignored; symmetric)
+*  @param[in] callback
+*    The callback to invoke for each parsed line part
+*/
+CPPASSIST_API void parseLine(const std::string & line, const std::string::size_type start_pos, const char delimiter, const char escapeCharacter, std::function<void(std::string && value)> callback);
+CPPASSIST_API void parseLine(const std::string & line, const std::string::size_type start_pos, const char delimiter, const char escapeCharacter, std::function<void(std::string && value, const int index)> callback);
+//@}
+
+
+} // namespace string
+} // namespace cppassist
---
+++ source/cppassist/source/string/processing.cpp
@@ -0,0 +1,69 @@
+
+#include <cppassist/string/processing.h>
+
+#include <cassert>
+
+#include <cppassist/string/manipulation.h>
+
+
+namespace cppassist
+{
+namespace string
+{
+
+
+void parseLine(const std::string & line, const std::string::size_type start_pos, const char delimiter, const char escapeCharacter, std::function<void(std::string && value, const int index)> callback)
+{
+    if (start_pos == std::string::npos)
+    {
+        // No rest of line
+        return;
+    }
+
+    int i = 0;
+    auto last_pos = start_pos;
+
+    // bool hasEscapeCharacter = line.find(escapeCharacter, last_pos);
+
+    for (auto current_pos = line.find(delimiter, last_pos);
+         current_pos != std::string::npos;
+         current_pos = line.find(delimiter, last_pos))
+    {
+        // escaped string
+        if (line.at(last_pos) == escapeCharacter)
+        {
+            // find correct closing escape character
+            if (line.at(current_pos - 1) != escapeCharacter)
+            {
+                current_pos = line.find(escapeCharacter, current_pos);
+
+                assert(current_pos == line.size() - 1 || line.at(current_pos+1) == delimiter);
+            }
+
+            callback(cppassist::string::trim(line.substr(last_pos + 1, current_pos - last_pos - 1)), i);
+
+            last_pos = current_pos + 1 + 1;
+            ++i;
+        }
+        else
+        {
+            callback(cppassist::string::trim(line.substr(last_pos, current_pos - last_pos)), i);
+
+            last_pos = current_pos + 1;
+            ++i;
+        }
+    }
+
+    callback(cppassist::string::trim(line.substr(last_pos)), i);
+}
+
+void parseLine(const std::string & line, const std::string::size_type start_pos, const char delimiter, const char escapeCharacter, std::function<void(std::string && value)> callback)
+{
+    parseLine(line, start_pos, delimiter, escapeCharacter, [callback](std::string && value, const int /*index*/) {
+        callback(std::move(value));
+    });
+}
+
+
+} // namespace string
+} // namespace cppassist
